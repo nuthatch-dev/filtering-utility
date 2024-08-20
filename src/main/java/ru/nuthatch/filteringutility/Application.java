4b6e1ed543725677c06d710702648afa297319f0
Eugene Elantsev <blinktreeman@gmail.com>
2024-08-21 01:16:56 +0700

    Parameters n setup done

--- src/main/java/ru/nuthatch/filteringutility/Application.java
+++ src/main/java/ru/nuthatch/filteringutility/Application.java
@@ -1,10 +1,11 @@
 package ru.nuthatch.filteringutility;
 
-import org.apache.commons.cli.*;
+import ru.nuthatch.filteringutility.common.SetupParameters;
 
 public class Application {
     public static void main(String[] args) {
 
-
+        SetupParameters setupParameters = new SetupParameters();
+        setupParameters.setup(args);
     }
 }
